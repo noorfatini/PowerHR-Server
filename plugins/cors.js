5000537caa8afdd11bd1fbca513c59294dfc1d20
Naim Jalil <muhammadnaim370@gmail.com>
2024-05-29 14:57:09 +0800

    Update Cors

--- plugins/cors.js
+++ plugins/cors.js
@@ -3,6 +3,7 @@ import fp from 'fastify-plugin';
 
 export default fp(async (fastify) => {
     fastify.register(cors, {
-        origin: '*',
+        origin: ['http://localhost:5173', 'https://power-hr.vercel.app', 'https://power-hr-development.vercel.app'],
+        credentials: true,
     });
 });
