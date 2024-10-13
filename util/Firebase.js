3ad75476c78cae693d6642a12e9d29b5c42d7a17
Naim Jalil <muhammadnaim370@gmail.com>
2024-10-13 14:45:28 +0800

    Fix Bug Firebase Cert

--- util/Firebase.js
+++ util/Firebase.js
@@ -17,17 +17,6 @@ const serviceAccount = {
     universal_domain: process.env.SERVICE_ACCOUNT_UNIVERSAL_DOMAIN,
 };
 
-const firebaseConfig = {
-    credential: cert(serviceAccount),
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
-
 // Initialize Firebase
 
 class Firebase {
@@ -43,6 +32,17 @@ class Firebase {
     constructor() {
         //Edit this line to use Firebase in test environment
         if (ENV !== 'test') {
+            const firebaseConfig = {
+                credential: cert(serviceAccount),
+                apiKey: process.env.FIREBASE_API_KEY,
+                authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+                messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+                appId: process.env.FIREBASE_APP_ID,
+                measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+            };
+
             initializeApp(firebaseConfig);
             this.bucket = getStorage().bucket();
         }
