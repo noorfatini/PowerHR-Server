f1e70b3e31f47d34238267d0a2a5a275427dabba
NOORFATINI FARHAH BINTI NOOR ADLY <noorfatinifarhah@graduate.utm.my>
2024-12-17 11:18:37 +0800

    Update resumeController.js

--- services/resume/resumeController.js
+++ services/resume/resumeController.js
@@ -5,9 +5,25 @@ class ResumeController {
         await Resume.create({ user: userId });
     }
 
-    async updateResume(resume) {
-        await Resume.findByIdAndUpdate(resume._id, resume);
+    async getResumeByUser(userId) {
+        return await Resume.findOne({ user: userId });
     }
+
+    async getAllResumes() {
+        return await Resume.find();
+    }
+
+    async updateResume(resume) {
+        return await Resume.findByIdAndUpdate(resume._id, resume, { new: true });
+    }
+
+    async deleteResume(resumeId) {
+        return await Resume.findByIdAndDelete(resumeId);
+    }
+    
+    // async updateResume(resume) {
+    //     await Resume.findByIdAndUpdate(resume._id, resume);
+    // }
 }
 
 export default ResumeController;
